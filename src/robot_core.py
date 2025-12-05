a2a3632fd92cc79f3eaab42b83807aa79fecc9de
Jeremy Leibs <jeremy@rerun.io>
2025-12-05 14:50:58 -0500

    Move all the python code under src

---
+++ src/robot_core.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+#
+
+import commands2
+import wpilib
+
+import teleop
+from robot_systems import RobotSystems
+
+
+class Robot(wpilib.TimedRobot):
+    def robotInit(self) -> None:
+        """Robot initialization function"""
+        self.robot_systems = RobotSystems()
+
+        self.robot_systems.init()
+
+        self.scheduler = commands2.CommandScheduler.getInstance()
+
+    def robotPeriodic(self):
+        try:
+            self.scheduler.run()
+        except Exception as e:
+            self.log.error(e)
+            self.nt.getTable("errors").putString("command scheduler", str(e))
+
+    def autonomousPeriodic(self) -> None:
+        pass
+
+    def teleopInit(self):
+        teleop.setup_controls(self.robot_systems)
+
+    def teleopPeriodic(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    wpilib.run(Robot)
