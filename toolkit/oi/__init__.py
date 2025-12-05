d4ff8ae5655431616b0daab7b421caafe675b9c7
Jeremy <jeremy@leibs.net>
2025-12-04 23:52:47 -0500

    Get rid of the toolkit/oi -- not needed

--- toolkit/oi/__init__.py
+++
@@ -1,3 +0,0 @@
-from toolkit.oi.buttons import DefaultButton, Button, AxisButton
-from toolkit.oi.controllermap import LogitechController, XBoxController
-from toolkit.oi.joysticks import Joysticks, JoystickAxis
